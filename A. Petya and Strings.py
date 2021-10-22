6624b7c437f960161aee39a8ecfb9e8f47f72749
carl <huyhn1321@gmail.com>
2021-10-23 02:17:03 +0900

    A. Petya and Strings.py

---
+++ A. Petya and Strings.py
@@ -0,0 +1,13 @@
+'''
+https://codeforces.com/problemset/problem/112/A
+'''
+
+string1 = input().lower()
+string2 = input().lower()
+
+if string1 == string2:
+    print(0)
+elif string1 < string2:
+    print(-1)
+else:
+    print(1)
